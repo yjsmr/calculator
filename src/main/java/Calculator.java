fed382c301742449418a0a0b193811df5f032e1d
Yonas Romell <yonas.romell@live.se>
2017-12-29 12:39:55 +0100

    Take three inputs

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,11 +6,35 @@ public class Calculator {
         Calculator calculator = new Calculator();
         Scanner sc = calculator.createScanner();
         Integer a;
+        Integer b;
+        Character o;
         try {
+            System.out.println("Enter first number:");
             a = sc.nextInt();
-            System.out.println(a);
+            System.out.println("Enter second number:");
+            b = sc.nextInt();
+            System.out.println("Enter operation:");
+            o = sc.next().charAt(0);
+            System.out.println(o);
+
         } catch (InputMismatchException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public Integer performOperation(Character operand, int a, int b) {
+        switch (operand) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                System.out.println("incorrect operand");
+                return null;
         }
     }
 
